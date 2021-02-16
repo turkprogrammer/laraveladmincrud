1a3605bcd0685bb03d72ccd75baabefbc07f20d9
DESKTOP-7AU1H3Q\Yunus <ostbat43@gmail.com>
2021-02-16 18:03:42 +0500

    вывод категорий

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -15,7 +15,12 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+
+        $categories = Category::orderBy('created_at', 'desc')->get();
+
+        return view('admin.category.index', [
+            'categories' => $categories
+        ]);
     }
 
     /**
